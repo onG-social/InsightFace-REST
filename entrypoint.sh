bdfb719fdcf67440c82f94ac79e4799899c786dd
ckramerica <chris@somee.ai>
2026-03-01 15:48:40 -0800

    Update entrypoint.sh
    Signed-off-by: ckramerica <chris@somee.ai>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 set -e
-# echo Preparing models...
-# python -m if_rest.prepare_models
 
-echo Starting InsightFace-REST using $NUM_WORKERS workers.
+# Set safe defaults if Railway variables not defined
+LOG_LEVEL=${LOG_LEVEL:-info}
+NUM_WORKERS=${NUM_WORKERS:-2}
+PORT=${PORT:-8080}
 
-exec gunicorn --log-level $LOG_LEVEL\
-     -w $NUM_WORKERS\
-     -k uvicorn.workers.UvicornWorker\
-     --keep-alive 60\
-     --timeout 60\
-     if_rest.api.main:app -b 0.0.0.0:$PORT
+echo "Starting InsightFace-REST using $NUM_WORKERS workers on port $PORT."
+
+exec gunicorn \
+  --log-level "$LOG_LEVEL" \
+  -w "$NUM_WORKERS" \
+  -k uvicorn.workers.UvicornWorker \
+  --keep-alive 60 \
+  --timeout 60 \
+  if_rest.api.main:app \
+  -b 0.0.0.0:"$PORT"
